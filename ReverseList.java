60cd5665459be080f8af43d3b3fb44a740829064
pinpailoy <pinpailoy@gmail.com>
2022-01-13 23:26:23 +0800

    update new code

---
+++ ReverseList.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class ReverseList {
+    public List<String> startReverse(List<String> text) {
+        Collections.reverse(text);
+        return text;
+    }
+
+}
--- searchText.java
+++ searchText.java
@@ -1,12 +1,52 @@
 
 import java.util.*;
+
 public class searchText {
-	public static void main(String[] args)  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter palindrome || reverseList : ");
+        String text = sc.next();
+        switch (text) {
+            case "palindrome":
+                selectPalindrome();
+                break;
+            case "reverseList":
+                selectReverseList();
+                break;
+            default:
+                System.out.println("Thanks !");
+
+        }
+
+    }
+
+    public static void selectPalindrome() {
         EasyPalindrome easy = new EasyPalindrome();
         System.out.println("test palindrome");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter text : ");
         String sc2 = sc.next();
         if (easy.check(sc2)) {
+            System.out.println(sc2 + " is a palinDrome ");
+        } else {
+            System.out.println(sc2 + " is NOT a palinDrome");
+        }
+    }
+
+    public static void selectReverseList() {
+        ReverseList reverseList = new ReverseList();
+        System.out.println("Plese insert total number of list:");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int total = Integer.parseInt(sc.next());
+        List<String> test = new ArrayList<String>();
+        for (int i = 0; i < total; i++) {
+            System.out.println("Enter text : ");
+            String t = sc.next();
+            test.add(t);
+        }
+        System.out.println("Here's all the list before reverse: " + test.toString());
+        System.out.println("After reverse " + reverseList.startReverse(test).toString());
+
     }
 }
